2ade879958fbfe2ae0aa896bc077cf6b33a4cd6b
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-25 11:59:33 +0300

    save

---
+++ src/components/Member.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img :src="require(`@/assets/img/${path}`)" alt="">
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  name: 'member',
+  props: {
+    path: String
+  }
+}
+
+</script>
--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    Members
+   <Member></Member>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Member from '../components/Member.vue'
 export default {
-  name: 'members'
+  name: 'members',
+  components: {
+    Member
+  }
 }
 </script>
