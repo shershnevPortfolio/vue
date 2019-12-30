db2a40cb0a3305e79fae0185772165d2d4c5d0d8
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-30 17:57:52 +0300

    save

---
+++ src/components/ImageBlock..vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img :src="require(`@/assets/img/${path}`)" alt="">
+  </div>
+</template>
+
+<style lang="sass">
+</style>
+
+<script>
+export default {
+
+  name: 'ImageBlock',
+  props: {
+    path: String
+  }
+}
+
+</script>
