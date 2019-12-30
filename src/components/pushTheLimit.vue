a632bb5c893d5ed04e2981cb380b12a236cef590
danil shershnev <shershnev.danil2000@gmail.com>
2019-12-30 15:11:43 +0300

    save commit

---
+++ src/components/pushTheLimit.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="limit">
+    <img :src="require(`@/assets/img/limit.png`)" alt="">
+    <img :src="require(`@/assets/img/cars.png`)" alt="">
+  </div>
+</template>
+
+<style lang="sass">
+  @import './src/assets/components/social.sass'
+</style>
+
+<script>
+export default {
+
+  name: 'pushTheLimit',
+  props: {
+    path: String
+  }
+}
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 
 <template>
   <div class="page-inner">
-    <MapBlock path='map.png'></MapBlock>
+    <div class="content">
+      <pushTheLimit></pushTheLimit>
+      <MapBlock path='map.png'></MapBlock>
+    </div>
     <Social ></Social>
   </div>
 </template>
@@ -10,13 +13,15 @@
 
 import MapBlock from '../components/MapBlock.vue'
 import Social from '../components/Social.vue'
+import pushTheLimit from '../components/pushTheLimit.vue'
 
 export default {
   name: 'home',
 
   components: {
     MapBlock,
-    Social
+    Social,
+    pushTheLimit
   }
 }
 </script>
